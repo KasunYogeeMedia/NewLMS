a9772f58b955b827f6b4ae6867a0de2924b1e2ec
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-09-21 17:37:33 +0530

    9.21

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-  All rights reserved.
-  <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b> 3.2.0
-  </div>
+    <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> <a target="_blank" href="">Guruniwasa LMS</a>.</strong>
+    All rights reserved. Developd by <a target="_blank" href="https://mozita.digital/">Mozita Digital</a>
+    <!-- <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div> -->
 </footer>
 
 <!-- Control Sidebar -->
