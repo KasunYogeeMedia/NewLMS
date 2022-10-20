61793b64a288e94ec95ab042db1052d0fe32d183
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-10-20 11:16:39 +0530

    test

--- index.php
+++ index.php
@@ -9,29 +9,34 @@ require_once 'header.php';
         <div class="row">
             <div class="col-lg-6 col-sm-6">
                 <!-- small box -->
-                <div class="small-box bg-info">
-                    <div class="inner">
-                        <h3>Free Education</h3>
-                        <h4 class="sinhala">නොමිලේ අධ්‍යාපනය</h4>
+                <a class="crd_inner" href="user">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>Free Education</h3>
+                            <h4 class="sinhala">නොමිලේ අධ්‍යාපනය</h4>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="user" class="small-box-footer">Get Started <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-                    <a href="user" class="small-box-footer">Get Started <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-6 col-sm-6">
                 <!-- small box -->
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3>Smart Science <br> Student Login</h3>
+                <a class="crd_inner" href="user">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>Smart Science <br> Student Login</h3>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="teacher" class="small-box-footer">Get Started <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-                    <a href="teacher" class="small-box-footer">Get Started <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
+                </a>
+
             </div>
             <!-- ./col -->
         </div>
