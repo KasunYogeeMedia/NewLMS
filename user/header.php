9530b38b8c46fee1fbd02d42038e73602d753c4e
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-10-21 10:41:57 +0530

    test

--- user/header.php
+++ user/header.php
@@ -30,6 +30,8 @@
     <link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
     <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- Custom user css -->
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
