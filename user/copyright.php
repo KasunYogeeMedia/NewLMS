84dbc4db837bc07b6846fb1c8a3c9effe974e930
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-01-05 13:33:27 +0530

    header footer watsappa

--- user/copyright.php
+++ user/copyright.php
@@ -2,7 +2,8 @@
     <strong>Copyright &copy; <script>
             document.write(new Date().getFullYear());
         </script> <a target="_blank" href="">Smart Science LMS</a>.</strong>
-    All rights reserved. Developd by <a target="_blank" href="https://mozita.digital/">Mozita Digital</a> &nbsp; | &nbsp;<span class="d-inline">අභිමන් ගුරුතුමාගේ පන්‍ති පිළිබඳ විස්තර ලබා ගැනීමට දරුවාගේ නම, ශ්‍රේණිය සහ මාධ්‍ය 0772879970 අංකයට WhatsApp කරන්න. &nbsp;&nbsp;</span>
-        <img class="img_icon" src="../dist/img/watsapp+icon.png" alt="">
+    All rights reserved. Developd by <a target="_blank" href="https://mozita.digital/">Mozita Digital</a> &nbsp; | &nbsp;<a href="https://wa.me/94772879970" target="_blank">
+    <span class="d-inline text-dark">අභිමන් ගුරුතුමාගේ පන්‍ති පිළිබඳ විස්තර ලබා ගැනීමට දරුවාගේ නම, ශ්‍රේණිය සහ මාධ්‍ය 0772879970 අංකයට WhatsApp කරන්න. &nbsp;&nbsp;</span>
+    <img class="img_icon" src="../dist/img/watsapp+icon.png" alt="">
+    </a>
 </footer>
-
